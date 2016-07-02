b78e87d39ee457ec9da9c9ba82be11e3ce1b72a4
PeinLavita <yby920424@163.com>
2016-07-02 09:12:11 +0800

    no2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("branch 1");
+        System.out.println("branch 2");
     }
 }
